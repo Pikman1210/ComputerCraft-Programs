aaacf5f4f150eec338c12087ae78d0927fff159b
Pikman1210 <72507796+Pikman1210@users.noreply.github.com>
2023-07-29 19:41:52 -0500

    Create remote.lua

---
+++ Elevator Controller/remote.lua
@@ -0,0 +1,48 @@
+---@diagnostic disable: undefined-global, undefined-field
+local argument1 = arg[1]
+local argument2 = arg[2]
+
+rednet.open("back")
+
+--Checks if something is actually a number (yippe)
+local function checknumber(num)
+    if type(num) == "number" then
+    	return true
+    else
+    	return false
+    end
+end
+
+local function interpret(floor)
+    if floor/4 <= 1 then
+        rednet.broadcast("ElevationSection1")
+    elseif floor/4 <= 2 then
+        rednet.broadcast("ElevationSection2")
+    elseif floor/4 <= 3 then
+        rednet.broadcast("ElevationSection3")
+    elseif floor/4 <= 4 then
+        rednet.broadcast("ElevationSection4")
+    else
+        print("Invalid Floor")
+    end
+end
+
+local function help()
+    print("Usage:")
+    print("null")
+end
+
+if arg[1] == nil then
+    print("Use \"elevatorInterpreter help\" for usage")
+elseif arg[1] == "help" then
+    help()
+elseif arg[1] == "call" or arg[1] == "send" then
+    local number = tonumber(argument2)
+    if checknumber(number) == true then
+        interpret(number)
+    else
+        help()
+    end
+elseif arg[1] == "toggle" then
+    print("toggle elevator")
+end
