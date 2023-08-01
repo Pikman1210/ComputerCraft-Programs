143fafddabe9e04f42325924579d76a3aceac3f0
Pikman1210 <72507796+Pikman1210@users.noreply.github.com>
2023-07-31 21:03:14 -0500

    Update ResourceElevatorManager.lua

--- Elevator Controller/ResourceElevatorManager.lua
+++ Elevator Controller/ResourceElevatorManager.lua
@@ -3,43 +3,49 @@
 local ground = "top"
 local function gotoSurface()
     redstone.setOutput(ground, true)
-    os.sleep(15)
+    os.sleep(1)
     redstone.setOutput(ground, false)
+    os.sleep(15)
 end
 
 local processing = "right"
 local function gotoProcessing()
     redstone.setOutput(processing, true)
-    os.sleep(20)
+    os.sleep(1)
     redstone.setOutput(processing, false)
+    os.sleep(20)
 end
 
 local iron = "back"
 local function gotoIron()
     redstone.setOutput(iron, true)
-    os.sleep(15)
+    os.sleep(1)
     redstone.setOutput(iron, false)
+    os.sleep(15)
 end
 
 local gold = "left"
 local function gotoGold()
     redstone.setOutput(gold, true)
-    os.sleep(15)
+    os.sleep(1)
     redstone.setOutput(gold, false)
+    os.sleep(15)
 end
 
 local copper = "bottom"
 local function gotoCopper()
     redstone.setOutput(copper, true)
-    os.sleep(15)
+    os.sleep(1)
     redstone.setOutput(copper, false)
+    os.sleep(15)
 end
 
 local zinc = "front"
 local function gotoZinc()
     redstone.setOutput(zinc, true)
-    os.sleep(15)
+    os.sleep(1)
     redstone.setOutput(zinc, false)
+    os.sleep(15)
 end
 
 print("Starting at ground (G)")
